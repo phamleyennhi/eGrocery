935799af6e6e340dc1abbd86d8b07a7a964aba5e
Jakub Podmokly <oaklab.nyuad@nyu.edu>
2021-04-25 15:44:23 +0400

    Feedback token

--- egrocery/src/components/Feedback.js
+++ egrocery/src/components/Feedback.js
@@ -13,17 +13,21 @@ class Feedback extends Component {
         feedback_db: []
       }
       // this.setState({ token: this.props.context });
-      const { user } = props.context;
-      console.log(user);
   }
 
-  async componentDidMount() {
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwNmFmYjJmYjc5NWEyYzZiZjU1OWQ4YiIsImlhdCI6MTYxOTMwMzEzNiwiZXhwIjoxNjE5Mzg5NTM2fQ.LpVw0dWMI915r3_DBX7VpKHKpJ_OpHsIENHuWQAA6CA";
-    const res = await axios.get('https://se-egrocery.herokuapp.com/api/admin/feedback',
-                                        {headers: {"x-access-token": token}});
-    this.setState({feedback_db: res.data});
-    console.log(this.state.feedback_db);
+  async componentDidUpdate() {
+
+        if (this.props.context !== null && this.state.feedback_db.length == 0) {
+
+            const { user } = this.props.context;
+            const res = await axios.get('https://se-egrocery.herokuapp.com/api/admin/feedback',
+                                                {headers: {"x-access-token": user.token}});
+            this.setState({feedback_db: res.data});
+
+        }
+
   }
+
 
   render(){
     return (
