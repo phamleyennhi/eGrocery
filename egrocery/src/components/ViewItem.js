ae35f29459c24a8813ff8ad12a5b5e20f44a1d9c
Nhi Pham <npl243@nyu.edu>
2021-04-26 13:49:53 +0400

    display item, will need styling

--- egrocery/src/components/ViewItem.js
+++ egrocery/src/components/ViewItem.js
@@ -3,7 +3,7 @@ import withContext from "../withContext";
 import axios from 'axios';
 
 import 'bootstrap/dist/css/bootstrap.min.css'; //bootstrap
-import { Container } from 'reactstrap';
+import { Container, Col } from 'reactstrap';
 
 class ViewItem extends Component {
   constructor(props) {
@@ -24,17 +24,44 @@ class ViewItem extends Component {
       // this.state.products = this.props.context;
       console.log("getting item from database");
       const res = await axios.get('https://se-egrocery.herokuapp.com/api/product/' + this.state._id);
-      this.setState({product: res.data});
+      this.setState({product: res.data[0]});
       console.log(res.data);
     }
   }
 
   render(){
+    console.log(this.state.product);
     return (
       <>
+      {this.state.product !== null ?
+      (<div className="row">
+            <div className="col-12 mb-2">
+
+            <h4 className="text-end"> {this.state.product.name} <small className="text-secondary"> {"<"+this.state.product.category+"/" +this.state.product.subcategory+">"} </small></h4>
+                            </div>
+            
+            <div className="col-12">
+            <p style={{"white-space": "pre-line"}}> {this.state.product.description} </p>
+                        <hr className="mb-4"/>
+            </div>
+            <div className="col-12">
+            <p style={{"white-space": "pre-line"}}> {this.state.product.shortDesc} </p>
+                        <hr className="mb-4"/>
+            <p style={{"white-space": "pre-line"}}> {this.state.product.price} AED </p>
+                        <hr className="mb-4"/>
+            <p style={{"white-space": "pre-line"}}> {this.state.product.stock} left in stock </p>
+                        <hr className="mb-4"/>
+            <Col md="12" className="border-bottom p-0 product-image-wrapper">
+              <img className="img-fluid mx-auto p-2 product-image" src={this.state.product.url} alt="" />
+            </Col>
+            </div>
+
+        </div>)
+      : (
       <Container>
-        View item here!
+      Loading item
       </Container>
+      )}
       </>
     );
 }};
